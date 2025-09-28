0bd331ba4b96e7117f784ce410c8c6948eff12ac
Андрей Боев <a.boev.it@gmail.com>
2025-09-28 12:04:20 +0300

    UPD: ЕГЭ. Тема 4

--- ЕГЭ/Тема 4/Задания/Задание 4.py
+++ ЕГЭ/Тема 4/Задания/Задание 4.py
@@ -1,10 +1,6 @@
 answer = ...
 
 
-import datetime
-import hashlib
-from tests.conftest import add_result
+from tests.conftest import result_register
 if answer is not Ellipsis:
-    result = 1 if hashlib.md5(str(answer).encode()).hexdigest() == 'c74d97b01eae257e44aa9d5bade97baf' else 0
-    print("Верно" if result else "Неверно")
-    add_result(datetime.now().timestamp(), 4, 4, result)
+    print(result_register(4, 4, answer, 'c74d97b01eae257e44aa9d5bade97baf'))

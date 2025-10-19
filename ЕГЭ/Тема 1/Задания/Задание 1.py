29c08bfb86b3c12b5d835872d0bc4c2216eecbae
Даниил <uzzsr@mail.ru>
2025-10-19 17:34:31 +0300

    d

--- ЕГЭ/Тема 1/Задания/Задание 1.py
+++ ЕГЭ/Тема 1/Задания/Задание 1.py
@@ -13,3 +13,15 @@ answer = ...
 from tests.conftest import result_register
 if answer is not Ellipsis:
     print(result_register(1, 1, answer, '735b90b4568125ed6c3f678819b6e058'))
+
+    from itertools import permutations
+
+    table = '12 17 21 27 28 34 35 37 43 47 53 57 67 68 71 72 73 74 75 76 78 82 86 87'
+    graph = 'AB BA AC CA AD DA BC CB CD DC AE EA AF FA AG GA AH HA EF FE FG GF GH HG'
+
+    for p in permutations('ABCDEFGH'):
+        new_graph = table
+        for i in range(1, 9):
+            new_graph = new_graph.replace(str(i), p[i - 1])
+        if set(new_graph.split()) == set(graph.split()):
+            print(p)

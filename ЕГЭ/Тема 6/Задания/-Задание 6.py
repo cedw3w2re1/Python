9d28055ffbac34307353f9e34a2f58b4d61b6eef
Даниил <uzzsr@mail.ru>
2025-11-05 00:25:59 +0300

    Обновлен статус задания 6 темы 6. Задание решено Неверно

---
+++ ЕГЭ/Тема 6/Задания/-Задание 6.py
@@ -0,0 +1,50 @@
+# Решение
+from turtle import *
+tracer(0)
+koef = 20
+
+right(90)
+for i in range(2):
+    forward( 3 * koef)
+    left(90)
+    back(10 * koef)
+    left(90)
+
+up()
+
+for x in range(-koef, koef):
+    for y in range(-koef, koef):
+        goto(x * koef, y * koef)
+        dot(3)
+
+for i in range(1):
+    back(10 * koef)
+    right(90)
+    forward(8 * koef)
+    left(90)
+
+down()
+
+for i in range(2):
+    forward(16 * koef)
+    right(90)
+    forward(8 * koef)
+    right(90)
+
+exitonclick()
+
+
+
+
+
+
+
+
+
+answer = 18
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(6, 6, answer, 'eecca5b6365d9607ee5a9d336962c534'))

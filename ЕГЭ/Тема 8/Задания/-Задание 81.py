de3240526932be63c513ab62070723503befb009
Даниил <uzzsr@mail.ru>
2025-12-18 17:56:28 +0300

    Обновлен статус задания 81 темы 8. Задание решено Неверно

---
+++ ЕГЭ/Тема 8/Задания/-Задание 81.py
@@ -0,0 +1,24 @@
+# Решение
+import itertools
+
+list_v = itertools.product('РОСОМАХА', repeat = 8)
+n = 0
+
+for str in list_v:
+    line = ''.join(str)
+    if line.count('Р') == 1 and line.count('О') == 2 and line.count('С') == 1 and line.count('М') == 1 and line.count('Х') == 1 and line.count('А') == 2:
+        n += 1
+print(n)
+
+
+
+
+
+
+answer = 161280
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(8, 81, answer, '48aedb8880cab8c45637abc7493ecddd'))

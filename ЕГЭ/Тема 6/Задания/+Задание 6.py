977b061e63e530ef86a58b4d748aff736ca72a91
Даниил <uzzsr@mail.ru>
2025-12-28 13:51:55 +0300

    Обновлен статус задания 6 темы 6. Задание решено Верно

---
+++ ЕГЭ/Тема 6/Задания/+Задание 6.py
@@ -0,0 +1,52 @@
+# Решение
+
+from turtle import *
+tracer(0)
+k = 20
+
+
+for i in range(2):
+    fd(3*k)
+    lt(90)
+    bk(10*k)
+    lt(90)
+up()
+
+bk(10*k)
+rt(90)
+fd(8*k)
+lt(90)
+
+down()
+
+for i in range(2):
+    fd(16 * k)
+    rt(90)
+    fd(8 * k)
+    rt(90)
+up()
+for x in range(-k,k):
+    for y in range(-k,k):
+        goto(x*k, y*k)
+        dot(3)
+exitonclick()
+
+
+
+answer = 185
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(6, 6, answer, 'eecca5b6365d9607ee5a9d336962c534'))
+
+
+
+
+
+
+
+
+
+
